95df46f5a27f81e5360c450f7f1c284f50fe7a4f
3566844 <3566844@gmail.com>
2022-03-10 16:52:19 +0300

    test11

--- src/test/java/Lesson11Test.java
+++ src/test/java/Lesson11Test.java
@@ -3,16 +3,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import java.util.List;
 
-
 public class Lesson11Test {
-
-        @Test
+    @Test
     public void mySecondTest(){
-            WebDriver driver = new ChromeDriver();
-            driver.get("https://the-internet.herokuapp.com/");
-            List<WebElement> elements = driver.findElements(By.tagName("a"));
-//            driver.findElement(By.linkText(elements.get(8).getText())).click();
-            driver.findElement(By.linkText(elements.get(8).getText())).sendKeys(Keys.ENTER);
-        }
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        List<WebElement> elements = driver.findElements(By.tagName("a"));
+        driver.findElement(By.linkText(elements.get(8).getText())).click();
+//            driver.findElement(By.linkText(elements.get(8).getText())).sendKeys(Keys.ENTER);
+//            String login = "admin";
+//            driver.findElement(By.xpath("//*[contains(text(), 'Имя пользователя')]")).sendKeys(login);
+    }
 }
+
 
