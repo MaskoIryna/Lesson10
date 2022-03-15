0a0d8537dca634f7e384568e82be668b903c74e2
35668 <3566844@gmail.com>
2022-03-15 12:53:02 +0300

    test13Frame

--- src/test/java/Lesson13FrameTest.java
+++ src/test/java/Lesson13FrameTest.java
@@ -4,30 +4,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.security.Key;
+import java.time.Duration;
 
 
 public class Lesson13FrameTest {
     @Test
     public void FrameTest() {
         WebDriver driver = new ChromeDriver();
+
         driver.get("https://the-internet.herokuapp.com/");
         driver.findElement(By.xpath("//a[text()='Frames']")).click();
         driver.findElement(By.xpath("//a[text()='iFrame']")).click();
 
-//        WebElement elementEdit = driver.findElement(By.xpath("//span[text()='Edit']"));
-//        elementEdit.click();
-//        Actions builder = new Actions(driver);
-//        WebElement dd = driver.findElement(By.xpath("//div[text()='Select all']"));
-//
-//                builder.moveToElement(dd).sendKeys(Keys.ENTER).sendKeys(Keys.CONTROL + "X");
-//
-
-
+        String xpathText = "//body[@class='mce-content-body ']/p";
         driver.switchTo().frame("mce_0_ifr");
-        String xpathElement = "//body[@class='mce-content-body ' ]/p";
+        WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath(xpathText)));
 
-         driver.findElement(By.xpath(xpathElement)).clear();
-        driver.findElement(By.xpath(xpathElement)).sendKeys("Hello World");
+        driver.findElement(By.xpath(xpathText)).clear();
+        driver.findElement(By.xpath(xpathText)).sendKeys("Hello World");
+        driver.findElement(By.xpath(xpathText)).sendKeys(Keys.chord(Keys.CONTROL,"a"));
+        driver.switchTo().defaultContent();
+        driver.findElement(By.xpath("//button[@title='Italic']")).click();
+        driver.switchTo().frame("mce_0_ifr");
+        String expected = driver.findElement(By.tagName("p")).getText();
+        driver.switchTo().defaultContent();
+        Assert.assertEquals("Hello World",expected);
+
+
     }
 }
