1d16d872eee13ab49638a6b84c0c95f0942491d4
Ирина Масько <98155269+MaskoIryna@users.noreply.github.com>
2022-03-22 09:06:11 +0300

    Update RubberDucksPage.java

--- src/test/java/pageObject/FactoryPage/pages/RubberDucksPage.java
+++ src/test/java/pageObject/FactoryPage/pages/RubberDucksPage.java
@@ -10,7 +10,7 @@ import java.util.Collections;
 import java.util.List;
 
 
-public class RubberDucksPage extends TestBase {
+public class RubberDucksPage {
     WebDriver driver;
     @FindBy(linkText = "Rubber Ducks" )
     private WebElement bottomRubberDucks;
